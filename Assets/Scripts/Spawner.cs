06a05d94827c41403bc19ba5a7cef0a3e574a62f
weikunou <962264443@qq.com>
2021-02-21 13:33:43 +0800

    Init project

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 方块生成器类
+/// </summary>
+public class Spawner : MonoBehaviour
+{
+    /// <summary>
+    /// 方块预制体
+    /// </summary>
+    public GameObject[] blocks;
+
+
+    void Start()
+    {
+        GenerateBlock();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 生成方块
+    /// </summary>
+    void GenerateBlock()
+    {
+        int i = Random.Range(0, blocks.Length);
+
+        Instantiate(blocks[i], transform.position, Quaternion.identity);
+    }
+}
