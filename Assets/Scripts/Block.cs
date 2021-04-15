b58df09afa21f452da103918bef42e2471e64bd3
weikunou <962264443@qq.com>
2021-04-15 11:57:18 +0800

    Make the blocks inside border

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 方块类
+/// </summary>
+public class Block : MonoBehaviour
+{
+    /// <summary>
+    /// 上次下落的时间
+    /// </summary>
+    float lastFall;
+
+    void Start()
+    {
+        if(!isValidGridPos())
+        {
+            Debug.Log("游戏结束");
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// 移动方块
+    /// </summary>
+    void Move()
+    {
+        // 按键控制
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+
+            if (isValidGridPos())
+            {
+
+            }
+            else
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+
+            if (isValidGridPos())
+            {
+
+            }
+            else
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Rotate(0, 0, -90);
+
+            if (isValidGridPos())
+            {
+
+            }
+            else
+            {
+                transform.Rotate(0, 0, 90);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Time.time - lastFall >= 1)
+        {
+            transform.position += new Vector3(0, -1, 0);
+
+            if (isValidGridPos())
+            {
+
+            }
+            else
+            {
+                transform.position += new Vector3(0, 1, 0);
+            }
+
+            lastFall = Time.time;
+        }
+    }
+
+    /// <summary>
+    /// 是否在有效的格子内
+    /// </summary>
+    /// <returns></returns>
+    bool isValidGridPos()
+    {
+        foreach(Transform child in transform)
+        {
+            Vector2 v = Grid.roundVec2(child.position);
+
+            if(!Grid.insideBorder(v))
+            {
+                return false;
+            }
+
+            if (Grid.grid[(int)v.x, (int)v.y] != null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 网格类
+/// </summary>
+public class Grid : MonoBehaviour
+{
+    /// <summary>
+    /// 网格宽度
+    /// </summary>
+    public static int w = 8;
+
+    /// <summary>
+    /// 网格高度
+    /// </summary>
+    public static int h = 20;
+
+    /// <summary>
+    /// 网格二维数组
+    /// </summary>
+    public static Transform[,] grid = new Transform[w, h];
+
+    /// <summary>
+    /// 四舍五入转化三维向量成二维向量
+    /// </summary>
+    /// <param name="vector3">三维向量</param>
+    /// <returns></returns>
+    public static Vector2 roundVec2(Vector3 vector3)
+    {
+        return new Vector2(Mathf.Round(vector3.x), Mathf.Round(vector3.y));
+    }
+
+    /// <summary>
+    /// 判断方块是否在边界内
+    /// </summary>
+    /// <param name="pos">位置</param>
+    /// <returns></returns>
+    public static bool insideBorder(Vector2 pos)
+    {
+        // 满足条件则在边框内
+        return ((int)pos.x >= 0 &&
+                (int)pos.x < w &&
+                (int)pos.y >= 0);
+    }
+}
