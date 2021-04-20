a89e8cc54cfed39f369549181162a7d025735e7c
weikunou <962264443@qq.com>
2021-04-20 21:09:53 +0800

    Add score and game over

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// UI 管理器类
@@ -23,6 +24,26 @@ public class UIManager : MonoBehaviour
     /// </summary>
     public Text timer;
 
+    /// <summary>
+    /// 分数文本
+    /// </summary>
+    public Text scoreText;
+
+    /// <summary>
+    /// 游戏结束窗口
+    /// </summary>
+    public GameObject gameover;
+
+    /// <summary>
+    /// 游戏是否结束
+    /// </summary>
+    bool isEnd = false;
+
+    /// <summary>
+    /// 分数
+    /// </summary>
+    public int score;
+
     void Start()
     {
         // 游戏开始时的时间
@@ -31,6 +52,11 @@ public class UIManager : MonoBehaviour
 
     void Update()
     {
+        if (isEnd)
+        {
+            return;
+        }
+
         // 游戏运行的时间
         time = Time.time - startTime;
 
@@ -42,5 +68,48 @@ public class UIManager : MonoBehaviour
         string strTime = string.Format("{0:00} : {1:00}", minutes, seconds);
 
         timer.text = strTime;
+    }
+
+    /// <summary>
+    /// 增加分数
+    /// </summary>
+    public void IncreaseScore()
+    {
+        score++;
+
+        if(score < 10)
+        {
+            scoreText.text = string.Format("000{0}", score);
+        }
+        else if (score < 100)
+        {
+            scoreText.text = string.Format("00{0}", score);
+        }
+        else if (score < 1000)
+        {
+            scoreText.text = string.Format("0{0}", score);
+        }
+        else
+        {
+            scoreText.text = string.Format("{0}", score);
+        }
+    }
+
+    /// <summary>
+    /// 游戏结束
+    /// </summary>
+    public void GameOver()
+    {
+        gameover.SetActive(true);
+
+        isEnd = true;
+    }
+
+    /// <summary>
+    /// 重新开始游戏
+    /// </summary>
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
